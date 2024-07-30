39c3fa5fdb9da1e7712752e2f2202e5bd496595d
aemarcinkowska <61382068+aemarcinkowska@users.noreply.github.com>
2024-07-30 15:44:07 +0200

    Update task-2.js

--- task-2.js
+++ task-2.js
@@ -1,7 +1,6 @@
 const getUsersWithFriend = (users, friendName) => {
-    return users.filter(user => user.friends.includes(friendName));
+  return users.filter(user => user.friends && user.friends.includes(friendName));
 };
-
 
 const allUsers = [
     {
@@ -33,3 +32,29 @@ const allUsers = [
       friends: ["Goldie Gentry", "Briana Decker"]
     }
   ];
+
+  console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+// [
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+// [
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"]
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"]
+//   }
+// ]
+
+console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
